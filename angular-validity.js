970c078c3a1ace9e61f21fafc455652bd2232162
Jason Pierce <jason@2toad.com>
2015-11-25 19:02:34 -0800

    validityDirective: use third-party UUID lib when available

--- angular-validity.js
+++ angular-validity.js
@@ -362,21 +362,25 @@
         }
     ]);
 
-    function uuid() {
+    // Use third-party UUID lib when available, otherwise fallback to
+    // internal v4 UUID generator
+    var uuid = window.uuid || function () {
         var l = "0123456789abcdef",
             m = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx",
             u = "",
             i = 0,
             rb = Math.random() * 0xffffffff | 0;
+
         while (i++ < 36) {
             var c = m[i - 1],
                 r = rb & 0xf,
                 v = c == "x" ? r : (r & 0x3 | 0x8);
             u += (c == "-" || c == "4") ? c : l[v];
-            rb = i % 8 == 0 ? Math.random() * 0xffffffff | 0 : rb >> 4;
+            rb = i % 8 === 0 ? Math.random() * 0xffffffff | 0 : rb >> 4;
         }
+
         return u;
-    }
+    };
 }());
 
 (function () {
