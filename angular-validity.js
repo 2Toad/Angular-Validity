8bf93532e9610e49e8327a0b75a93794b2fb2572
Jason Pierce <jason@2toad.com>
2017-02-04 21:00:34 -0800

    Simplify uuid function

--- angular-validity.js
+++ angular-validity.js
@@ -363,29 +363,14 @@
             function setValidityId(ctrl, $element) {
                 ctrl.$vid = uuid();
                 $element.attr("vid", ctrl.$vid);
+
+                function uuid() {
+                    return Math.random().toString(36).substring(2, 15) +
+                        Math.random().toString(36).substring(2, 15);
+                }
             }
         }
     ]);
-
-    // Use third-party UUID lib when available, otherwise fallback to
-    // internal v4 UUID generator
-    var uuid = window.uuid || function () {
-        var l = "0123456789abcdef",
-            m = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx",
-            u = "",
-            i = 0,
-            rb = Math.random() * 0xffffffff | 0;
-
-        while (i++ < 36) {
-            var c = m[i - 1],
-                r = rb & 0xf,
-                v = c == "x" ? r : (r & 0x3 | 0x8);
-            u += (c == "-" || c == "4") ? c : l[v];
-            rb = i % 8 === 0 ? Math.random() * 0xffffffff | 0 : rb >> 4;
-        }
-
-        return u;
-    };
 }());
 
 (function () {
