5477f034dcd67c98e52ba5a2b3a52f8aab2825a6
Jason Pierce <jpierce@surgeforward.com>
2017-02-19 11:41:54 -0800

    Add validity-provider tests (wip)

--- test/validity-provider.spec.js
+++ test/validity-provider.spec.js
@@ -1,15 +1,60 @@
 'use strict';
 
 describe('validity', function() {
-  var provider;
+  var validityProvider;
 
   beforeEach(module('ttValidity'));
 
   beforeEach(inject(function(validity) {
-    provider = validity;
+    validityProvider = validity;
   }));
 
-  it('provider should exist', function() {
-    expect(provider).toBeDefined();
+  it('validityProvider should exist', function() {
+    expect(validityProvider).toBeDefined();
   });
+
+  describe('callbacks function', function() {
+    it('should exist', function() {
+      expect(validityProvider.callbacks).toBeDefined();
+    });
+
+    it('should not return validityProvider', function() {
+      var callbacks = validityProvider.callbacks();
+      expect(callbacks).not.toEqual(validityProvider);
+      expect(callbacks).toBeDefined(callbacks.valid);
+      expect(callbacks).toBeDefined(callbacks.invalid);
+    });
+
+    it('should return validityProvider', function() {
+      var result = validityProvider.callbacks({foo: 'bar'});
+      expect(result).toEqual(validityProvider);
+    });
+  });
+
+  describe('options function', function() {
+    it('should exist', function() {
+      expect(validityProvider.options).toBeDefined();
+    });
+
+    it('should not return validityProvider', function() {
+      var result = validityProvider.options();
+      expect(result).not.toEqual(validityProvider);
+    });
+
+    it('should return validityProvider', function() {
+      var result = validityProvider.options({});
+      expect(result).toEqual(validityProvider);
+    });
+  });
+
+  // describe('reset function', function() {
+  //   it('should exist', function() {
+  //     expect(validityProvider.reset).toBeDefined();
+  //   });
+  //
+  //   it('should return validityProvider', function() {
+  //     var result = validityProvider.reset({});
+  //     expect(result).toEqual(validityProvider);
+  //   });
+  // });
 });
